8223a089f4e77b38dc539c8d1f8748946188607b
Sahithkumar-31 <sahithkumar.ksrs@gmail.com>
2024-10-05 13:14:12 +0530

    created_utils and src

---
+++ src/pred_code/utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import langchain
+from langchain_community.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import SequentialChain
+from langchain.chains import LLMChain
+
+print("This is running sucesfully")
